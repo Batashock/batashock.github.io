5a45b46b8ae6b2110c32d04e3634f92bb9803c3c
Batashock <73817895+Batashock@users.noreply.github.com>
2023-01-08 18:13:31 +1100

    Testing to make sure i'm not stupid

---
+++ radsite/scripts.js
@@ -0,0 +1,20 @@
+    function OPENTHEDOOR(h) {
+	var dooropen = new Audio('dooropen.wav');
+	dooropen.play();
+	document.getElementById("door").src=h;
+    setTimeout(removedoor, 3000);
+	setTimeout(hereticanthem, 3000);
+	setTimeout(revealpage, 3000);
+}
+	function removedoor() {
+	document.getElementById("door").style.width="0px";
+}
+	function hereticanthem() {
+	MIDIjs.play('TheHereticAnthem.mid');
+}
+	function revealpage() {
+	document.getElementById("WELCOMETO").style.opacity="100%";
+}
+	function secretCounter() {
+	var Counter = 0
+	if Counter = 50
